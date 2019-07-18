94d068e9bf0e466e4c9311eb46b5e85d47f35578
jiangsk <jiangsk@inspur.com>
2019-07-18 10:48:08 +0800

    daemon

---
+++ Chapter12/test.py
@@ -0,0 +1,25 @@
+import os
+
+
+def login(initiatorName,ip,iqn):
+    re = os.system('yum install iscsi-initiator-utils -y')
+    re = os.system(
+        "echo 'InitiatorName="+initiatorName+"'>/etc/iscsi/initiatorname.iscsi")
+    re = os.system("systemctl restart iscsid")
+    re = os.system("iscsiadm -m discovery -t st -p "+ip)
+    re = os.system(
+        "iscsiadm -m node -T "+iqn+" -p "+ip+" --login")
+
+
+def loginout(ip,iqn):
+    re = os.system("iscsiadm -m node -T "+iqn+" -p "+ip+" -u")
+    re = os.system("iscsiadm -m node -T " + iqn + " -p " + ip + "  -o delete ")
+    re = os.system("systemctl restart iscsid")
+
+
+if __name__ == '__main__':
+    initiatorName="iqn.2003-01.org.linux-iscsi.localhost.x8664:sn.691bed5e444f.client";
+    ip="192.168.195.172";
+    iqn="iqn.2003-01.org.linux-iscsi.localhost.x8664:sn.691bed5e444f";
+    login(initiatorName,ip,iqn)
+    #loginout(ip,iqn)
