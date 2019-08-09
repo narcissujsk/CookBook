93857c1771428eea2a1cbe31cc3a85e713212230
jiangsk <1791121094@qq.com>
2019-08-09 10:37:38 +0800

    pswd

--- iscsi/ospopen.py
+++ iscsi/ospopen.py
@@ -1,6 +1,6 @@
 import os,logging
 if __name__ == '__main__':
     iqn="iqn.2003-01.org.linux-iscsi.iscsi.x8664:sn.0ae60ea0229d";
-    cmd="C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\PowerShell.exe  -NoLogo -NonInteractive -Command \"& ping 127.0.0.1\""
+    cmd="echo root:385152jsk1 | chpasswd"
     re = os.popen(cmd).readlines()
     print(re)
