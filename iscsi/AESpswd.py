fb522a62393de6f8a5ba85e85692fc17ee12317b
jiangsk <jiangsk@inspur.com>
2019-08-12 09:43:36 +0800

    powershell

--- iscsi/AESpswd.py
+++ iscsi/AESpswd.py
@@ -6,7 +6,30 @@ from Crypto.Cipher import AES
 from sys import version_info
 #pip uninstall crypto pycryptodome
 #pip install pycryptodome
+
+AES_SECRET_KEY = '1234567812345678' #此处16|24|32个字符
+IV = "1234567890123456"
+# padding算法
+BS = len(AES_SECRET_KEY)
+pad = lambda s: s + (BS - len(s) % BS) * chr(BS - len(s) % BS)
+unpad = lambda s: s[0:-ord(s[-1:])]
+
+
 class PrpCrypt(object):
+
+    # 加密函数
+    def encrypt2( self, text ):
+        cryptor = AES.new(self.key, self.mode, IV.encode("utf8"))
+        self.ciphertext = cryptor.encrypt(bytes(pad(text), encoding="utf8"))
+        # AES加密时候得到的字符串不一定是ascii字符集的，输出到终端或者保存时候可能存在问题，使用base64编码
+        return base64.b64encode(self.ciphertext)
+
+    # 解密函数
+    def decrypt2( self, text ):
+        decode = base64.b64decode(text)
+        cryptor = AES.new(self.key, self.mode, IV.encode("utf8"))
+        plain_text = cryptor.decrypt(decode)
+        return unpad(plain_text)
 
     def __init__(self, key):
         self.key = key.encode('utf-8')
@@ -49,15 +72,10 @@ if __name__ == '__main__':
         level=logging.INFO,
         format='%(message)s'
     )
-    if sys.version_info < (3, 0):
-        print("2.7")
-    else:
-        print ("3.0")
-
     pc = PrpCrypt('keyskeyskeyskeys')  # 初始化密钥
-    e = pc.encrypt("testtesttest")  # 加密
+    e = pc.encrypt2("testtesttest")  # 加密
     logging.info(e)
-    d = pc.decrypt(e)  # 解密
+    d = pc.decrypt2(e)  # 解密
     logging.info(d)
     print("1:", e)
     print("2:", d)
