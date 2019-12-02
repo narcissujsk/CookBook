c3a67da98bb0fee54f1ab9f30c535c1754a7ff68
narcissujsk <1791121094@qq.com>
2019-12-01 22:22:00 -0800

    s

--- narcissujsk_agent/run.sh
+++ narcissujsk_agent/run.sh
@@ -3,4 +3,4 @@ cp ./agent/daemon.py /usr/local/lib/narcissujsk/
 cp ./agent/run.sh /usr/local/lib/narcissujsk/
 cp ./agent/stop.sh /usr/local/lib/narcissujsk/
 cp ./agent/narcissujsk.service  /lib/systemd/system/
-systemctl narcissujsk start
+service narcissujsk start
