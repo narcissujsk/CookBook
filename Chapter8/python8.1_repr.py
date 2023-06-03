b7f6f42dcbca31f7035393969e8f7cd9e104016a
jiangsk <1791121094@qq.com>
2023-06-03 11:20:20 +0800

    commit

--- Chapter8/python8.1_repr.py
+++ Chapter8/python8.1_repr.py
@@ -1,7 +1,7 @@
 class Pair:
-    def __init__(self,x,y):
-        self.x=x
-        self.y=y
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
     # __repr__的标准做法是使他产生的字符串满足eval(repr(x))===x
     def __repr__(self):
@@ -11,20 +11,19 @@ class Pair:
         return '({0.x!s},{0.y!s})'.format(self)
 
     def __eq__(self, other):
-        if self.x==other.x and self.y==other.y:
+        if self.x == other.x and self.y == other.y:
             return True
-        else:return False
+        else:
+            return False
 
 
-
-
-if __name__=='__main__':
+if __name__ == '__main__':
     print("start")
-    p=Pair(3,4)
+    p = Pair(3, 4)
     print(repr(p))
     print('p is {0!r}'.format(p))
     print('p is {0}'.format(p))
-    p2=eval(repr(p))
+    p2 = eval(repr(p))
     print("p2:")
     print(p2)
     if eval(repr(p)) == p:
